add9b5f41b4b318760fa135bbf96636a73df0d24
qq <q@q.com>
2021-01-19 05:46:51 +0500

    testing us-west-2 region

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -35,62 +35,62 @@ data "aws_subnet_ids" "subnets" {
 # }
 
 
-
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
- // version                = "~> 1.9"
-}
-
-module "in28minutes-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "in28minutes-cluster"
-  cluster_version = "1.17"
-  #subnets         = ["subnet-cf219eee", "subnet-5c3f8203", "subnet-0a86413b", "subnet-6598f26b"] #CHANGE # Donot choose #subnet from us-east-1e
-  subnets = data.aws_subnet_ids.subnets.ids
-  vpc_id          = aws_default_vpc.default.id
-  #vpc_id         = "vpc-1234556abcdef"
-
-  node_groups = [
-    {
-      instance_type = "t2.micro"
-      max_capacity  = 5
-      desired_capacity = 3
-      min_capacity  = 3
-    }
-  ]
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.in28minutes-cluster.cluster_id
-}
-
-
-# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
-# ServiceAccount needs permissions to create deployments
-# and services in default namespace
-resource "kubernetes_cluster_role_binding" "example" {
-  metadata {
-    name = "fabric8-rbac"
-  }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "default"
-    namespace = "default"
-  }
-}
+#
+#
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   load_config_file       = false
+#  // version                = "~> 1.9"
+# }
+#
+# module "in28minutes-cluster" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   cluster_name    = "in28minutes-cluster"
+#   cluster_version = "1.17"
+#   #subnets         = ["subnet-cf219eee", "subnet-5c3f8203", "subnet-0a86413b", "subnet-6598f26b"] #CHANGE # Donot choose #subnet from us-east-1e
+#   subnets = data.aws_subnet_ids.subnets.ids
+#   vpc_id          = aws_default_vpc.default.id
+#   #vpc_id         = "vpc-1234556abcdef"
+#
+#   node_groups = [
+#     {
+#       instance_type = "t2.micro"
+#       max_capacity  = 5
+#       desired_capacity = 3
+#       min_capacity  = 3
+#     }
+#   ]
+# }
+#
+# data "aws_eks_cluster" "cluster" {
+#   name = module.in28minutes-cluster.cluster_id
+# }
+#
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.in28minutes-cluster.cluster_id
+# }
+#
+#
+# # We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# # ServiceAccount needs permissions to create deployments
+# # and services in default namespace
+# resource "kubernetes_cluster_role_binding" "example" {
+#   metadata {
+#     name = "fabric8-rbac"
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "default"
+#     namespace = "default"
+#   }
+# }
 
 # Needed to set the default region
 provider "aws" {
